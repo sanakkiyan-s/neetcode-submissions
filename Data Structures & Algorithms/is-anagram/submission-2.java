1347c8d675af3919c9467f1276553171bef4ecaf
Sanakkiyan <sanakiyan739@gmail.com>
2026-07-24 22:18:57 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+                if (s.length() != t.length()) {
+            return false;
+        }
+        int[] arr = new int[26];
+        for (int i=0; i<s.length(); i++){
+            arr[s.charAt(i)-'a']+=1;
+            arr[t.charAt(i)-'a']-=1;
+        }
+        for(int val: arr){
+            if (val!=0) return false;
+        }
+        return true ;
+
+    }
+}
